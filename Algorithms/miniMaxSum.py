131b22f403ac189695515474d72c0ef93b057a06
droid-225 <aryanrishit1@gmail.com>
2025-12-29 23:44:01 +0530

    Revised solution for miniMaxSum

--- Algorithms/miniMaxSum.py
+++ Algorithms/miniMaxSum.py
@@ -1,17 +1,8 @@
 def miniMaxSum(arr):
-    for i in range(1, 5):
-        key = arr[i]
-        j = i - 1
+    total = sum(arr)
+    min = total - max(arr)
+    max = total - min(arr)
 
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    
-    min = arr[0] + arr[1] + arr[2] + arr[3]
-    max = arr[1] + arr[2] + arr[3] + arr[4]
-
-    print(arr)
     print(min, max)
 
 if __name__ == '__main__':
